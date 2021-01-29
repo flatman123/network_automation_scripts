3bd0f42a855c5aabb8018f921a616f5754b94532
Jeffrey McIntyre <redeyecoding@gmail.com>
2021-01-29 11:57:05 -0500

    moved scripts to own folder

---
+++ python_scripts/README.md
@@ -0,0 +1,3 @@
+# network_automation_scripts
+
+Here you will find a bunch of automation scripts created in python.
---
+++ python_scripts/list_of_devices.py
@@ -0,0 +1,16 @@
+import csv
+import random
+
+with open('./device_info.csv','a', newline='') as csvFile:
+    HEADERS = ['IPADDRESS', 'DEVICE', 'USERNAME']
+    DEVICES = ['Cisco', 'Juniper', 'SonicWall']
+    IP_ADDRESS_PREFIX = '192.168.0.'
+
+    write_info = csv.writer(csvFile)
+    write_info.writerow(HEADERS)
+
+    for octet in range(1,60):
+        random_device = random.randint(0, len(DEVICES))
+        device_info = f'192.168.0.{octet}, {DEVICES[random_device - 1]}, admin'.split(',')
+        write_info.writerow(device_info)
+        
